face4feac292f47e95e81df755681f04ed40a12d
Ayaz Sarwar <ayaz.sarwar@zepta.com.pk>
2021-11-24 12:13:18 +0500

    Implement login method

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,18 +6,36 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
 
-  constructor(public auth: AuthService, public router:Router) { }
+  constructor(public auth: AuthService, public router: Router) {}
 
-  attempt(){
-    this.auth.login({ email: this.form.controls['email'].value, password: this.form.controls['password'].value });
+  attempt() {
+    this.auth
+      .login({
+        email: this.form.controls['email'].value,
+        password: this.form.controls['password'].value,
+      })
+      .subscribe({
+        next: (res) => {
+          this.auth.loggedIn$.next(true);
+          localStorage.setItem('access_token', res.token);
+          this.auth.token$.next(res.token);
+          this.auth.getUser().subscribe({
+            next: (user) => {
+              this.auth.user$.next(user);
+              this.router.navigate(['/'], { replaceUrl: true });
+            },
+            error: (e) => console.error(e),
+          });
+        },
+        error: (e) => console.error(e),
+      });
   }
 }
